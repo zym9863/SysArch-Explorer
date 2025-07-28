395c0a48ff948ba69e7f85fb8f4943fd0cffef93
zym <202213093023@cuc.edu.cn>
2025-07-28 22:03:16 +0800

    Add Gradle wrapper files for Android project
    - Created gradle-wrapper.properties to specify Gradle distribution URL.
    - Added gradlew shell script for Unix-based systems to execute Gradle tasks.
    - Added gradlew.bat script for Windows to execute Gradle tasks.
    - Created settings.gradle to include the app module and apply Tauri settings.

---
+++ src-tauri/gen/android/app/src/main/java/com/sysarch_explorer/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sysarch_explorer.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
